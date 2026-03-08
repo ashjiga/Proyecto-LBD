0f56bd94342917c32d248bfb04963d2ade207e72
ashyvy <ashjg07@gmail.com>
2026-03-07 19:36:30 -0600

    Correccion de la Conexion

--- conexion.py
+++ conexion.py
@@ -1,32 +1,23 @@
 import oracledb
 import getpass
-import os
 import sys
 
-instant_client_dir = os.getenv("ORACLE_CLIENT")
-wallet_dir = os.getenv("ORACLE_WALLET")
-
+instant_client_dir = r"" #Entre las comillas deben poner la ruta del archivo Instant Client
+wallet_dir = r"" #Entre las comillas deben poner la ruta del archivo Wallet descomprimido
 dsn = "databaselbd_high"
 user = "ADMIN"
 
 def obtener_conexion():
     try:
-        if instant_client_dir:
-            oracledb.init_oracle_client(lib_dir=instant_client_dir)
+        oracledb.init_oracle_client(lib_dir=instant_client_dir)
     except Exception:
         pass
 
-    password = getpass.getpass(f"Ingrese la contraseña para {user}: ")
+    password = getpass.getpass(f"Ingresa la contraseña para {user}: ")
 
     try:
-        conn = oracledb.connect(
-            user=user,
-            password=password,
-            dsn=dsn,
-            config_dir=wallet_dir
-        )
+        conn = oracledb.connect(user=user, password=password, dsn=dsn, config_dir=wallet_dir)
         return conn
-
     except oracledb.DatabaseError as e:
         print("Error de conexión:", e)
         sys.exit(1)
