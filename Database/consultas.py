012f4477ce3f2affbf3ab763fc81da7431c9cad6
ashyvy <ashjg07@gmail.com>
2026-03-25 19:47:31 -0600

    Commit

--- Database/consultas.py
+++ Database/consultas.py
@@ -1,3 +1,4 @@
+#Ejemplo
 import oracledb
 from conexion import obtener_conexion
 
@@ -6,9 +7,12 @@ print("Conexión exitosa")
 
 with conn.cursor() as cursor:
     out = conn.cursor()
-    cursor.callproc("SP_SELECT_CLIENTES", [out])
+
+    cursor.callproc("GET_CLIENTES", [out])
+
     print("--- Lista de Clientes ---")
-for fila in out:
+    
+    for fila in out:
         print(fila)
 
 conn.close()
