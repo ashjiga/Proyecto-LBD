5650c2601579883564fd9e477758023670bab17f
ashyvy <ashjg07@gmail.com>
2026-04-14 00:10:55 -0600

    CSS Actualizaciones

--- programming/app.py
+++ programming/app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from clientes import clientes_bp
 
-app = Flask(__name__, template_folder="../views")
+app = Flask(__name__,
+template_folder="../views",
+static_folder="../css",
+static_url_path="/css")
 
 app.register_blueprint(clientes_bp)
 
 @app.route("/")
 def index():
-    return "<h1>Inicio</h1><a href='/clientes'>Ir a Clientes</a>"
-
+    return render_template("index.html")  
 if __name__ == "__main__":
     app.run(debug=True)
